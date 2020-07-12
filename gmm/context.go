08a578b2654b5f91d02feb6779aa10628a6b46e0
wujie <2878354193@qq.com>
2020-07-12 11:08:09 +0800

    create context

---
+++ gmm/context.go
@@ -0,0 +1,70 @@
+package gmm
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+type Context struct {
+	Write http.ResponseWriter
+	Req   *http.Request
+
+	Path   string
+	Method string
+
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Write:  w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = 200
+	c.Write.WriteHeader(code)
+}
+
+func (c *Context) setHeader(key string, value string) {
+	c.Write.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.setHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Write.Write([]byte(fmt.Sprintf(format, values)))
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.setHeader("Content-Type", "application.json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Write)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Write, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Write.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.Status(200)
+	c.Write.Write([]byte(html))
+}
